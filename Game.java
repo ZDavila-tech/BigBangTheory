4bbf82c85942733ccde150bba8ff8dad1b96ea79
ZDavila-tech <zoleda.davila@students.tamuk.edu>
2022-03-22 12:24:37 -0500

    Fixed Loop

--- Game.java
+++ Game.java
@@ -15,25 +15,31 @@ public class Game {
         System.out.println("Choose your weapon: ");
         Scanner input= new Scanner(System.in);
         String user_action = input.next();
+
         switch(user_action){
             case "Rock":
                 switch(computer_actions){
                     case "Rock":
                         System.out.println("You and your opponent picked the same weapon! You Tied!!");
+                        System.out.println("Score: " + score);
                     break;
                     case "Paper":
                         System.out.println("Paper covers Rock!! You Lose!!!!");
+                        System.out.println("Score: " + score);
                     break;
                     case "Scissors":
                         score +=1;
                         System.out.println("Rock crushes Scissors!!!! You Win!!!");
+                        System.out.println("Score: " + score);
                     break;
                     case "Lizard":
                         score +=1;
                         System.out.println("Rock crushes Lizard!!! You Win!!!");
+                        System.out.println("Score: " + score);
                     break;
                     case "Spock":
                         System.out.println("Spock vaporizes Rock!!! You Lose!!!");
+                        System.out.println("Score: " + score);
                     break;
                 }
             break;
@@ -42,19 +48,24 @@ public class Game {
                     case "Rock":
                         score +=1;
                         System.out.println("Paper covers Rock!! You Win!!!!");
+                        System.out.println("Score: " + score);
                     break;
                     case "Paper":
                         System.out.println("You and your opponent picked the same weapon! You Tied!!");
+                        System.out.println("Score: " + score);
                     break;
                     case "Scissors":
                         System.out.println("Scissors cuts Paper!!! You Lose!!!");
+                        System.out.println("Score: " + score);
                     break;
                     case "Lizard":
                         System.out.println("Lizard eats Paper!!! You Lose!!!");
+                        System.out.println("Score: " + score);
                     break;
                     case "Spock":
                         score +=1;
                         System.out.println("Paper disproves Spock!!! You Win!!!");
+                        System.out.println("Score: " + score);
                     break;
                 }
             break;
@@ -62,21 +73,26 @@ public class Game {
                 switch(computer_actions){
                     case "Rock":
                         System.out.println("Rock crushes Scissors!!!! You Lose!!!");
+                        System.out.println("Score: " + score);
                     break;
                     case "Paper":
                         score +=1;
                         System.out.println("Scissors cuts Paper!!! You Win!!!");
+                        System.out.println("Score: " + score);
                     break;
                     case "Scissors":
                         System.out.println("You and your opponent picked the same weapon! You Tied!!");
+                        System.out.println("Score: " + score);
                     break;
                     case "Lizard":
                         score +=1;
                         System.out.println("Scissors decapitates Lizard!!! You Win!!!");
+                        System.out.println("Score: " + score);
 
                     break;
                     case "Spock":
                         System.out.println("Spock smashes Scissors!!! You Lose");
+                        System.out.println("Score: " + score);
                     break;
                 }
             break;
@@ -84,20 +100,25 @@ public class Game {
                 switch(computer_actions){
                     case "Rock":
                         System.out.println("Rock crushes Lizard!!! You Lose!!!");
+                        System.out.println("Score: " + score);
                     break;
                     case "Paper":
                         score +=1;
                         System.out.println("Lizard eats Paper!!! You Win!!!");
+                        System.out.println("Score: " + score);
                     break;
                     case "Scissors":
                         System.out.println("Scissors decapitates Lizard!!! You Lose!!!");
+                        System.out.println("Score: " + score);
                     break;
                     case "Lizard":
                         System.out.println("You and your opponent picked the same weapon! You tied!!");
+                        System.out.println("Score: " + score);
                     break;
                     case "Spock":
                         score +=1;
                         System.out.println("Lizard poisons Spock!!! You Win!!!");
+                        System.out.println("Score: " + score);
                     break;
                 }
             break;
@@ -106,39 +127,48 @@ public class Game {
                     case "Rock":
                         score +=1;
                         System.out.println("Spock vaporizes Rock!!! You Win!!!");
+                        System.out.println("Score: " + score);
                     break;
                     case "Paper":
                         System.out.println("Paper disproves Spock!!! You Lose!!!");
+                        System.out.println("Score: " + score);
                     break;
                     case "Scissors":
                         score +=1;
                         System.out.println("Spock smashes Scissors!!! You Win!!!");
+                        System.out.println("Score: " + score);
                     break;
                     case "Lizard":
                         System.out.println("Lizard poisons Spock!!! You Lose!!!");
+                        System.out.println("Score: " + score);
                     break;
                     case "Spock":
                         System.out.println("You and your opponent picked the same weapon! You tied!!");
+                        System.out.println("Score: " + score);
                     break;
                 }
             break;
         }
         System.out.println("Would you like to play again? Y/N: ");
         String play_agn = input.next();
-        switch(play_agn){
+       /* switch(play_agn){
             case "Y":
                 play = true;
                 break;
             case "N":
+                System.out.println("Final Score: " + score);
+                score = 0;
                 play = false;
                 break;
+        }*/
+        if(play_agn.equals("Y")){
+            play = true;
+        }else{
+            System.out.println("Final Score: " + score);
+                score = 0;
+                play = false;
         }
-        
-        input.close();
-        
-    }
-    
-}
-
-    
+    }   
+       
+    }   
 }
